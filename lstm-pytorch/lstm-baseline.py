e22f99bb097acfdcc7965a8a636213e40ff7a22f
Jessica Yung <jessicayungyc@gmail.com>
2018-09-11 17:12:50 +0100

    docs(lstm-pytorch): add documentation and tidy code

--- lstm-pytorch/lstm-baseline.py
+++ lstm-pytorch/lstm-baseline.py
@@ -3,28 +3,35 @@ import torch.nn as nn
 from generate_data import *
 import matplotlib.pyplot as plt
 
-# data params
+#####################
+# Set parameters
+#####################
+
+# Data params
 noise_var = 0
 num_datapoints = 100
 test_size = 0.2
 num_train = int((1-test_size) * num_datapoints)
 
-# network params
+# Network params
 input_size = 20
+# If `per_element` is True, then LSTM reads in one timestep at a time.
 per_element = True
 if per_element:
     lstm_input_size = 1
 else:
     lstm_input_size = input_size
-h1 = 256
-D_out = 1
-learning_rate = 1e-2
+# size of hidden layers
+h1 = 32
+output_dim = 1
+num_layers = 2
+learning_rate = 1e-3
 num_epochs = 500
 dtype = torch.float
 
-# generate data
-# data = SineWaveData(num_datapoints, num_prev=input_size, test_size=test_size, max_t=100, amplitude=0.5, mean=0.5)
-
+#####################
+# Generate data
+#####################
 data = ARData(num_datapoints, num_prev=input_size, test_size=test_size, noise_var=noise_var, coeffs=fixed_ar_coefficients[input_size])
 
 # make training and test sets in torch
@@ -36,48 +43,62 @@ y_test = torch.from_numpy(data.y_test).type(torch.Tensor).view(-1)
 X_train = X_train.view([input_size, -1, 1])
 X_test = X_test.view([input_size, -1, 1])
 
-# build model
+#####################
+# Build model
+#####################
 
-class RNN(nn.Module):
+# Here we define our model as a class
+class LSTM(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, batch_size):
-        super(RNN, self).__init__()
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
+                    num_layers=2):
+        super(LSTM, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.batch_size = batch_size
-        print("Batch size: ", batch_size)
+        self.num_layers = num_layers
 
-        self.lstm = nn.LSTM(input_dim, hidden_dim)
-        self.linear = nn.Linear(hidden_dim, 1)  # 1 here is output size
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
 
     def init_hidden(self):
-        return (torch.zeros(1, 1, self.hidden_dim),
-                torch.zeros(1, 1, self.hidden_dim))
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
 
     def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both 
+        # have shape (num_layers, batch_size, hidden_dim).
         lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+        
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
         y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
         return y_pred.view(-1)
 
-model = RNN(1, 32, batch_size=num_train)
-"""
-model = torch.nn.Sequential(
-    torch.nn.LSTM(input_size=lstm_input_size, hidden_size=h1, num_layers=1, dropout=0),
-    torch.nn.ReLU(),
-    torch.nn.Linear(h1, out_features=D_out)
-)
-"""
+model = LSTM(lstm_input_size, h1, batch_size=num_train, output_dim=output_dim, num_layers=num_layers)
 
 loss_fn = torch.nn.MSELoss(size_average=False)
 
 optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+#####################
+# Train model
+#####################
+
 hist = np.zeros(num_epochs)
-# train model
+
 for t in range(num_epochs):
-    model.zero_grad()
+    # Initialise hidden state
+    # Don't do this if you want your LSTM to be stateful
     model.hidden = model.init_hidden()
-    # forward pass
+    
+    # Forward pass
     y_pred = model(X_train)
 
     loss = loss_fn(y_pred, y_train)
@@ -88,11 +109,15 @@ for t in range(num_epochs):
     # Zero out gradient, else they will accumulate between epochs
     optimiser.zero_grad()
 
-    # backward pass
+    # Backward pass
     loss.backward()
 
-    # update parameters
+    # Update parameters
     optimiser.step()
+
+#####################
+# Plot preds and performance
+#####################
 
 plt.plot(y_pred.detach().numpy(), label="Preds")
 plt.plot(y_train.detach().numpy(), label="Data")
